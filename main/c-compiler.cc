af1ffd768b289a0265117fc077143eeacdd87246
Samuel Faubert <samuel.faubert@mail.mcgill.ca>
2025-11-21 00:45:40 -0500

    functioning scanner

--- main/c-compiler.cc
+++ main/c-compiler.cc
@@ -1,7 +1,27 @@
 #include "../lexer/scanner.hpp"
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!" << std::endl;
+int main(int argc, char **argv) {
+  std::filesystem::path defaultPath = "examples/hello-world.c";
+  if (const char *workspace = std::getenv("BUILD_WORKSPACE_DIRECTORY")) {
+    defaultPath = std::filesystem::path(workspace) / defaultPath;
+  }
+
+  std::filesystem::path inputPath =
+      argc > 1 ? std::filesystem::path(argv[1]) : defaultPath;
+  std::ifstream inputFile(inputPath);
+  if (!inputFile.is_open()) {
+    std::cout << "File not found!" << std::endl;
+    return -1;
+  }
+  lexer::Scanner scanner(inputFile);
+
+  while (scanner.hasNext()) {
+    std::cout << scanner.next();
+  }
+  std::cout << std::endl;
   return 0;
 }
